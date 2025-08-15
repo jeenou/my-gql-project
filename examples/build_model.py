03fc8ac45cbbf8b4b25e767eed98b24e5495b456
Essi Nousiainen <essi.nousiainen@vtt.fi>
2025-08-15 11:10:29 +0200

    Added createNode and createProcess examples

--- examples/build_model.py
+++ examples/build_model.py
@@ -71,3 +71,58 @@ query {
 
 model_result = client.execute(model_query)
 print("Model data:", model_result)
+
+# Example mutation to add a node to the model
+add_node_mutation = gql("""
+mutation CreateNode {
+    createNode(node: {
+        name: "ExampleNode",
+        isCommodity: false,
+        isMarket: false,
+        isRes: false,
+        cost: [],
+        inflow: []
+    }) {
+        errors {
+            field
+            message
+        }
+    }
+}
+""")
+
+add_node_result = client.execute(add_node_mutation)
+print("CreateNode result:", add_node_result)
+
+# Example mutation to add a process to the model
+add_process_mutation = gql("""
+mutation CreateProcess {
+    createProcess(process: {
+        name: "ExampleProcess",
+        conversion: UNIT,
+        isCfFix: false,
+        isOnline: true,
+        isRes: false,
+        eff: 1.0,
+        loadMin: 0.0,
+        loadMax: 1.0,
+        startCost: 0.0,
+        minOnline: 0.0,
+        maxOnline: 10.0,
+        minOffline: 0.0,
+        maxOffline: 10.0,
+        initialState: true,
+        isScenarioIndependent: true,
+        cf: [],
+        effTs: []
+    }) {
+        errors {
+            field
+            message
+        }
+    }
+}
+""")
+
+add_process_result = client.execute(add_process_mutation)
+print("CreateProcess result:", add_process_result)
