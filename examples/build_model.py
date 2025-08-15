83edfb42331d59b13b031a1dbbeeb2ca32e1f122
Essi Nousiainen <essi.nousiainen@vtt.fi>
2025-08-15 11:22:46 +0200

    Added process and node group additions and market

--- examples/build_model.py
+++ examples/build_model.py
@@ -10,18 +10,18 @@ transport = RequestsHTTPTransport(
 client = Client(transport=transport, fetch_schema_from_transport=True)
 
 # Example query to fetch schema or model info
-query = gql("""
-    query {
-        __schema {
-            types {
-                name
-            }
-        }
-    }
-""")
+# query = gql("""
+#    query {
+#        __schema {
+#            types {
+#                name
+#            }
+#        }
+#    }
+#""")
 
-result = client.execute(query)
-print("Schema types:", result)
+#result = client.execute(query)
+#print("Schema types:", result)
 
 # Example mutation to create input data setup
 
@@ -126,3 +126,57 @@ mutation CreateProcess {
 
 add_process_result = client.execute(add_process_mutation)
 print("CreateProcess result:", add_process_result)
+
+# Example mutation to add a node group
+add_node_group_mutation = gql("""
+mutation CreateNodeGroup {
+    createNodeGroup(name: "ExampleProcessGroup") {
+        message
+    }
+}
+""")
+add_node_group_result = client.execute(add_node_group_mutation)
+print("CreateNodeGroup result:", add_node_group_result)
+
+# Example mutation to add a process group
+add_process_group_mutation = gql("""
+mutation CreateProcessGroup {
+    createProcessGroup(name: "ExampleProcessGroup") {
+        message
+    }
+}
+""")
+add_process_group_result = client.execute(add_process_group_mutation)
+print("CreateProcessGroup result:", add_process_group_result)
+
+# Example mutation to add a market to the model
+add_market_mutation = gql("""
+mutation CreateMarket {
+    createMarket(market: {
+        name: "ExampleMarket",
+        mType: ENERGY,
+        node: "ExampleNode",
+        processGroup: "ExampleProcessGroup",
+        direction: UP,
+        realisation: [],
+        reserveType: "ReserveType",
+        isBid: true,
+        isLimited: false,
+        minBid: 0.0,
+        maxBid: 100.0,
+        fee: 0.0,
+        price: [],
+        upPrice: [],
+        downPrice: [],
+        reserveActivationPrice: []
+    }) {
+        errors {
+            field
+            message
+        }
+    }
+}
+""")
+
+add_market_result = client.execute(add_market_mutation)
+print("CreateMarket result:", add_market_result)
